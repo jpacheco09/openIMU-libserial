8e17748b6cd997bd7876d16466fa3ed98957e100
jpacheco009 <jesuseuardo18@gmail.com>
2024-01-24 12:17:01 -0600

    Update script

--- src/main.cpp
+++ src/main.cpp
@@ -32,20 +32,9 @@ void unpackU4(uint8_t *idx, const void *buffer, T &package) {
 }
 
 int main() {
-    LibSerial::SerialPort serial_port;
     LibSerial::SerialStream serial_stream;
-
-    // open serial SerialPort
+    // define device path
     const std::string file_name("/dev/ttyUSB0");
-
-    /* serial_port.Open(file_name);
-     // configure serial port
-     serial_port.SetBaudRate(LibSerial::BaudRate::BAUD_115200);
-     serial_port.SetCharacterSize(LibSerial::CharacterSize::CHAR_SIZE_8);
-     serial_port.SetFlowControl(LibSerial::FlowControl::FLOW_CONTROL_NONE);
-     serial_port.SetParity(LibSerial::Parity::PARITY_NONE);
-     serial_port.SetStopBits(LibSerial::StopBits::STOP_BITS_1);
- */
     // open serial SerialPort
     serial_stream.Open(file_name);
     // configure serial port
@@ -56,68 +45,70 @@ int main() {
     serial_stream.SetStopBits(LibSerial::StopBits::STOP_BITS_1);
 
     // check for opened port
-    /*if (serial_port.IsOpen()) {
+    if (serial_stream.IsOpen()) {
         std::cout << "Port " << file_name << " opened Successfully\n";
     } else {
         std::cerr << "Error opening " << file_name << std::endl;
         return EXIT_FAILURE;
-    }*/
-    size_t timeout_milliseconds = 25;
+    }
+
     std::string rx_buffer;
     const uint8_t BUFFER_SIZE = 55;
     char input_buffer[BUFFER_SIZE];
-    memset(input_buffer, 0, sizeof(input_buffer));
-    // input_buffer.reserve(static_cast<size_t>(BUFFER_SIZE));
-    // serial_port.Read(input_buffer, BUFFER_SIZE, timeout_milliseconds);
+    std::memset(input_buffer, 0, sizeof(input_buffer));
 
     uint32_t time_ms = 0;
-    uint8_t time_idx = 0;
+    uint32_t packet_header = 0;
+    uint8_t buffer_idx = 0;
+
     float roll = 0;
     float pitch = 0;
     float heading = 0;
+
     float attitude_data[3] = {0.0, 0.0, 0.0};
     float linear_rate[3];
     float linear_accel[3];
 
-    uint8_t payload_data_init_idx = 0;
     while (true) {
         if (serial_stream.IsDataAvailable()) {
             serial_stream.read(input_buffer, BUFFER_SIZE);
-            if (((input_buffer[0] << 8) | (input_buffer[1])) == 0x5555) {
-                /* debug print buffer 
+            unpackU4<uint32_t>(&buffer_idx, &input_buffer[buffer_idx], packet_header);
+            printf("Header: %04X\n", packet_header);
+            if (packet_header == 0x32615555) {
+                buffer_idx++;
+                /* debug print buffer
                 for (char &it : input_buffer) printf("0x%02X ", (uint8_t)(it));
                 printf("\n");
-               */ 
-
-                payload_data_init_idx = 17;
-
-                time_idx = 5;
-                /*
-                roll = unpackFloat(&attitude_idx, &input_buffer[attitude_idx]);
-                pitch = unpackFloat(&attitude_idx, &input_buffer[attitude_idx]);
-                heading = unpackFloat(&attitude_idx, &input_buffer[attitude_idx]);
-                */
-                // retrive time_ms
-                unpackU4<uint32_t>(&time_idx, &input_buffer[time_idx], time_ms);
-                // retrive attitude_data
-                for (int j = 0; j < 3; j++)
-                    unpackU4<float>(&payload_data_init_idx, &input_buffer[payload_data_init_idx], attitude_data[j]);
-                // retrive linear_rate
-                for (int j = 0; j < 3; j++)
-                    unpackU4<float>(&payload_data_init_idx, &input_buffer[payload_data_init_idx], linear_rate[j]);
-                // retrive linear_accel_rate
-                for (int j = 0; j < 3; j++)
-                    unpackU4<float>(&payload_data_init_idx, &input_buffer[payload_data_init_idx], linear_accel[j]);
+               */
+                // unpack time_ms
+                unpackU4<uint32_t>(&buffer_idx, &input_buffer[buffer_idx], time_ms);
+                buffer_idx += 8;  // skip time_s
+                                  // upack RP"Y" attitude
+                for (auto it : attitude_data) {
+                    unpackU4<float>(&buffer_idx, &input_buffer[buffer_idx], it);
+                }
+                // unpack linear rate
+                for (auto it : attitude_data) {
+                    unpackU4<float>(&buffer_idx, &input_buffer[buffer_idx], it);
+                }
+                // unpack linear rate
+                for (auto it : linear_rate) {
+                    unpackU4<float>(&buffer_idx, &input_buffer[buffer_idx], it);
+                }
+                // unpack linear accel
+                for (auto it : linear_accel) {
+                    unpackU4<float>(&buffer_idx, &input_buffer[buffer_idx], it);
+                }
+                // I know, that's a repeated pattern... we shall be able to refactor it
 
                 /* print data debug */
                 printf("time[ms]: %u\t", time_ms);
                 printf("Roll: %f\t Pitch: %f \t Heading: %f\n", attitude_data[0], attitude_data[1], attitude_data[2]);
-                printf("xrate: %f\t yrate: %f \t zrate: %f\n", linear_rate[0], linear_rate[1],linear_rate[2]);
+                printf("xrate: %f\t yrate: %f \t zrate: %f\n", linear_rate[0], linear_rate[1], linear_rate[2]);
                 printf("xaccel: %f\t yaccel: %f \t zaccel: %f\n", linear_accel[0], linear_accel[1], linear_accel[2]);
-                                
             }
 
-        } else {
+        } else {  // not needed, however we could admin cpu time by setting a timeout
             continue;
         }
     }
