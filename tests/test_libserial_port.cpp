878ff84ba0321b440965334a28a7df85cf556052
jpacheco009 <jesuseuardo18@gmail.com>
2024-05-20 16:46:59 -0600

    [Open Port] Failed to open correct port

--- tests/test_libserial_port.cpp
+++ tests/test_libserial_port.cpp
@@ -5,5 +5,8 @@
 TEST_CASE("open_imu::Libserial can interface with the serial device", "[LibSerialPort]") {
     SECTION("Initialize providing a Port") { open_imu::LibSerialPort serial_port("/dev/ttyUSB0"); }
     SECTION("Initialize empty constructor") { open_imu::LibSerialPort serial_port; }
-
+    SECTION("Open method works for correct port") {
+        open_imu::LibSerialPort serial_port("/dev/ttyUSB0");
+        REQUIRE(serial_port.Open() == true);
+    }
 }  // TEST_CASE open port
